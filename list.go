cd3a87d796ce1bd6c715ec9dd8b388222c7e5347
digisnow <y.aki.taji.1003@gmail.com>
2015-08-18 16:39:19 +0900

    リスト操作

---
+++ list.go
@@ -0,0 +1,24 @@
+//リストの作成と要素の追加を行うサンプルコード
+package main
+import (
+    "container/list"
+    "fmt"
+)
+
+func main() {
+
+    //空のリストを作成
+    l := list.New()
+
+    //リストに要素を追加
+    l.PushBack("a")
+    l.PushBack("b")
+    l.PushBack(3)
+
+    //リストの要素をイテレート
+    for e := l.Front(); e != nil; e = e.Next() {
+
+        //Value関数で要素が得られる
+        fmt.Println(e.Value)
+    }
+}
