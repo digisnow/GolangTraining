16a4cfc3d4a6ad55022e3a7533f794fafd40913d
digisnow <y.aki.taji.1003@gmail.com>
2015-08-30 14:54:16 +0900

    IPアドレス取得

---
+++ lookup.go
@@ -0,0 +1,22 @@
+//ホスト名からIPアドレスを取得するサンプルコード
+package main
+import (
+    "fmt"
+    "net"
+    "os"
+)
+
+func main() {
+
+    //ホスト名をIPアドレスに変換
+    addrs, err := net.LookupHost("www.google.com")
+
+    //エラーチェック
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    //取得したIPアドレスを出力
+    fmt.Println("%q\n", addrs)
+}
