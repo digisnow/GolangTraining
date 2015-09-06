644663398f16bd560d19abe522de676f6dea17e1
digisnow <y.aki.taji.1003@gmail.com>
2015-09-06 19:01:08 +0900

    ログ出力

---
+++ log.go
@@ -0,0 +1,13 @@
+//標準エラー出力にログを出力するサンプルコード
+package main
+import (
+    "log"
+)
+
+func main() {
+
+    //標準エラー出力にログを出力
+    log.Print("test", 1, 2, 3)
+    log.Println("test", 1, 2, 3)
+    log.Printf("test %d %d %d", 1, 2, 3)
+}
